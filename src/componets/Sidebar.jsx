163195c9deaa3959447651bd0c693be339d534c2
Antônio Thiago Neto <atn.coding@gmail.com>
2023-09-12 23:06:22 -0300

    Finalizando Sidebar

--- src/componets/Sidebar.jsx
+++ src/componets/Sidebar.jsx
@@ -1,4 +1,8 @@
+import {PencilLine } from 'phosphor-react'
+
+
 import styles from './Sidebar.module.css'
+
 
 
 export function Sidebar() {
@@ -7,12 +11,15 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/Antonio-coding.png" />
+
                 <strong>Antonio Thiago</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
